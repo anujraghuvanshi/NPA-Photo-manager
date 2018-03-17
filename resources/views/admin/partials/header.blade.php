9599b84ad282fa875375e9b87c79acce022f9d31
Anuj Singh <2685singh@gmail.com>
2018-03-17 21:27:31 +0530

    Logo Changed

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,9 +1,9 @@
 <div id="header">
     <div class="container">
         <div class="row">
-            <div class="logo">
-                <a href="index.html" title="">
-                    <img src="{{ asset('img/logo.png') }}" alt="Logo" />
+            <div class="" style="text-align: center;">
+                <a href="" title="">
+                    <img src="{{ asset('img/logo.png') }}" alt="Logo" style="height: 120px" />
                 </a>
             </div>
         </div>
